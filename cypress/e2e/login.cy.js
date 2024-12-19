5e2160c38ad449dcbe4f06c9802a2cb33659c395
xntknk <ex@eklektik-web.com>
2024-12-19 11:34:43 +0700

    test new yml

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,30 +1,38 @@
 import LoginPage from '../pages/LoginPage';
 
+let data;
+
 describe('Login Page Tests', () => {
   beforeEach(() => {
     LoginPage.visit(); // Visit the login page before each test
   });
+  before(() => {
+    // Load user data from a fixture file ONCE before all tests
+    cy.fixture(Cypress.env('fixtureFile')).then((userData) => {
+      data = userData; // Correctly assign to the declared variable
+    });
+  });
 
   it('should log in with valid credentials', () => {
-    cy.fixture(Cypress.env('fixtureFile')).then((userData) => {
-      LoginPage.enterUsername(userData.validUser.username);
-      LoginPage.enterPassword(userData.validUser.password);
+    
+      LoginPage.enterUsername(data.validUser.username);
+      LoginPage.enterPassword(data.validUser.password);
       LoginPage.clickLoginButton();
 
       // Verify successful login
       cy.url().should('not.include', '/login');
-    });
+    
   });
 
   it('should display an error message for invalid credentials', () => {
-    cy.fixture(Cypress.env('fixtureFile')).then((userData) => {
-      LoginPage.enterUsername(userData.invalidUser.username);
-      LoginPage.enterPassword(userData.invalidUser.password);
+    
+      LoginPage.enterUsername(data.invalidUser.username);
+      LoginPage.enterPassword(data.invalidUser.password);
       LoginPage.clickLoginButton();
 
       // Verify error message
       LoginPage.verifyErrorMessage('Username or password incorrect.');
-    });
+  
   });
 
   it('should require username and password fields', () => {
@@ -42,25 +50,25 @@ describe('Login Page Tests', () => {
     // Verify redirection to forgot password page
     cy.url().should('include', '/reset-password');
   });
-   // it('should display an error for an empty username and valid password', () => {
-  //   cy.fixture(Cypress.env('fixtureFile')).then((userData) => {
+  //  it('should display an error for an empty username and valid password', () => {
+    
   //     LoginPage.clearUsername();
-  //     LoginPage.enterPassword(userData.validUser.password);
+  //     LoginPage.enterPassword(data.validUser.password);
   //     LoginPage.clickLoginButton();
 
   //     // Verify validation message for username
   //     LoginPage.verifyUsernameFieldValidation('username is required');
-  //   });
+  
   // });
 
   // it('should display an error for a valid username and empty password', () => {
-  //   cy.fixture(Cypress.env('fixtureFile')).then((userData) => {
-  //     LoginPage.enterUsername(userData.validUser.username);
+    
+  //     LoginPage.enterUsername(data.validUser.username);
   //     LoginPage.clearPassword();
   //     LoginPage.clickLoginButton();
 
   //     // Verify validation message for password
   //     LoginPage.verifyPasswordFieldValidation('password is required');
-  //   });
+    
   // });
 });
