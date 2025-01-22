78eccd49cd4188e4126b5b827046ec9c764f3bba
xntknk <ex@eklektik-web.com>
2025-01-22 17:17:47 +0700

    CRUD ver.2

--- cypress/e2e/UserCRUD.cy.js
+++ cypress/e2e/UserCRUD.cy.js
@@ -3,32 +3,28 @@ import UserPage from "../pages/UserPage";
 let login;
 let user; // Correct variable name for user data
 
-describe("User CRUD", () => {
-  before(() => {
-    // Load login data from a fixture file ONCE before all tests
-    cy.fixture(Cypress.env("fixtureFile")).then((loginData) => {
-      login = loginData; // Assign login data
-    });
-
-    // Load user data from userData.json fixture file
-    cy.fixture("userData.json").then((userData) => {
-      user = userData; // Assign user data
-    });
+beforeEach(() => {
+  // Log in before each test
+  cy.login(login.validUser.username, login.validUser.password);
+});
+before(() => {
+  // Load login data from a fixture file ONCE before all tests
+  cy.fixture(Cypress.env("fixtureFile")).then((loginData) => {
+    login = loginData; // Assign login data
   });
 
-  beforeEach(() => {
-    // Log in before each test
-    cy.login(login.validUser.username, login.validUser.password);
+  // Load user data from userData.json fixture file
+  cy.fixture("userData.json").then((userData) => {
+    user = userData; // Assign user data
   });
+});
 
-  it("Create User", () => {
+describe("Create User", () => {
+ 
+
+  it("Create user with a valid information", () => {
     UserPage.visit(); // Visit User page
     UserPage.clickCreateBtn(); // Click Create button
-
-    // Check Required Fields
-    UserPage.clickSaveBtn(); // Click Save button without entering data
-    UserPage.reqFieldCheck(); // Check for required field validations
-
     // Enter Info and Create User
     UserPage.enterFname(user.newUser.firstName); // First name
     UserPage.enterLname(user.newUser.lastName); // Last name
@@ -41,22 +37,84 @@ describe("User CRUD", () => {
     // UserPage.toggleSwitch(); // Optional toggle
     UserPage.clickSaveBtn(); // Save
     cy.wait(2000);
+    UserPage.checkIfDataCreated(`${user.newUser.firstName} ${user.newUser.lastName}`);
+
+  });
+  it('If leave require field warning message should be displayed', () => {
+    UserPage.visit();
+    UserPage.clickCreateBtn();
+    UserPage.clickSaveBtn();
+    UserPage.reqFieldCheck();
+  });
+  it('If enter wrong email format it should have warning message', () => {
+    UserPage.visit();
+    UserPage.clickCreateBtn();
+    UserPage.enterFname(user.newUser.firstName); // First name
+    UserPage.enterLname(user.newUser.lastName); // Last name
+    UserPage.enterMobileNo(user.newUser.mobileNumber); // Mobile number
+    UserPage.enterEmail(user.newUser.invalidEmail); // Email
+    UserPage.selectRole(user.newUser.role); // Role
+    UserPage.enterDept(user.newUser.department); // Department
+    UserPage.enterSubDept(user.newUser.subDepartment); // Sub-department
+    UserPage.enterPosition(user.newUser.position); // Position
+    // UserPage.toggleSwitch(); // Optional toggle
+    UserPage.clickSaveBtn(); // Save
+    UserPage.emailFormatWarning();
+  });
+  it('If email is already used warning messgae should be displayed', () => {
+    UserPage.visit(); // Visit User page
+    UserPage.clickCreateBtn(); // Click Create button
+    // Enter Info and Create User
+    UserPage.enterFname(user.newUser.firstName); // First name
+    UserPage.enterLname(user.newUser.lastName); // Last name
+    UserPage.enterMobileNo(user.newUser.mobileNumber2); // Mobile number
+    UserPage.enterEmail(user.newUser.email); // Email
+    UserPage.selectRole(user.newUser.role); // Role
+    UserPage.enterDept(user.newUser.department); // Department
+    UserPage.enterSubDept(user.newUser.subDepartment); // Sub-department
+    UserPage.enterPosition(user.newUser.position); // Position
+    // UserPage.toggleSwitch(); // Optional toggle
+    UserPage.clickSaveBtn(); // Save
+    UserPage.emailDupMsg();
   });
 
+  it('If mobile number is already used warning messgae should be displayed', () => {
+    UserPage.visit(); // Visit User page
+    UserPage.clickCreateBtn(); // Click Create button
+    // Enter Info and Create User
+    UserPage.enterFname(user.newUser.firstName); // First name
+    UserPage.enterLname(user.newUser.lastName); // Last name
+    UserPage.enterMobileNo(user.newUser.mobileNumber); // Mobile number
+    UserPage.enterEmail(user.newUser.email2); // Email
+    UserPage.selectRole(user.newUser.role); // Role
+    UserPage.enterDept(user.newUser.department); // Department
+    UserPage.enterSubDept(user.newUser.subDepartment); // Sub-department
+    UserPage.enterPosition(user.newUser.position); // Position
+    // UserPage.toggleSwitch(); // Optional toggle
+    UserPage.clickSaveBtn(); // Save
+    UserPage.mobileDupMsg();
+  });
+});
+describe('Edit User', () => {
   it("Edit User", () => {
-    UserPage.visit(); // Navigate to User page
-    UserPage.clickEditBtn(`${user.newUser.firstName} ${user.newUser.lastName}`); // Click Edit button
-    UserPage.enterFname(user.editUser.firstName); // Edit First name
-    UserPage.enterLname(user.editUser.lastName); // Edit Last name
-    UserPage.enterMobileNo(user.editUser.mobileNumber); // Edit Mobile number
-    UserPage.editRole(user.editUser.role); // Edit Role
-    UserPage.enterDept(user.editUser.department); // Edit Department
-    UserPage.enterSubDept(user.editUser.subDepartment); // Edit Sub-department
-    UserPage.enterPosition(user.editUser.position); // Edit Position
-    UserPage.toggleSwitch(); // Toggle Enable/Disable
-    UserPage.clickSaveBtn(); // Save changes
-  });
+  UserPage.visit(); // Navigate to User page
+  UserPage.clickEditBtn(`${user.newUser.firstName} ${user.newUser.lastName}`); // Click Edit button
+  UserPage.enterFname(user.editUser.firstName); // Edit First name
+  UserPage.enterLname(user.editUser.lastName); // Edit Last name
+  UserPage.enterMobileNo(user.editUser.mobileNumber); // Edit Mobile number
+  UserPage.editRole(user.editUser.role); // Edit Role
+  UserPage.enterDept(user.editUser.department); // Edit Department
+  UserPage.enterSubDept(user.editUser.subDepartment); // Edit Sub-department
+  UserPage.enterPosition(user.editUser.position); // Edit Position
+  // UserPage.toggleSwitch(); // Toggle Enable/Disable
+  UserPage.clickSaveBtn(); // Save changes
+  cy.wait(2000);
+  UserPage.checkIfDataEdited(`${user.editUser.firstName} ${user.editUser.lastName}`);
 
+});
+});
+
+describe('Delete User', () => {
   it("Delete User", () => {
     UserPage.visit(); // Navigate to User page
     UserPage.clickDelBtn(user.deleteUser.email); // Click Delete button
@@ -64,5 +122,7 @@ describe("User CRUD", () => {
     cy.wait(2000);
     UserPage.clickDelBtn(user.deleteUser.email); // Click Delete again
     UserPage.confiirmDel(); // Confirm delete
+    cy.wait(3000);
+    UserPage.checkIfDataDeleted(`${user.editUser.firstName} ${user.editUser.lastName}`);
   });
 });
--- cypress/pages/UserPage.js
+++ cypress/pages/UserPage.js
@@ -14,25 +14,70 @@ class UserPage {
     cy.get("button span").contains("Save").click();
   }
 
+  checkIfDataCreated(username) {
+    cy.get("tr td:nth-child(2)").then(($cells) => {
+      const found = Array.from($cells).some((cell) => cell.textContent.trim() === username);
+  
+      // Assert that the username exists in the table
+      expect(found).to.be.true;
+    });
+  }
+  
+  
+
+  checkIfDataEdited(username){
+    cy.get("tr td:nth-child(2)").then(($cells) => {
+      const found = Array.from($cells).some((cell) => cell.textContent.trim() === username);
+  
+      // Assert that the username exists in the table
+      expect(found).to.be.true;
+    });
+  }
+
+  checkIfDataDeleted(username){
+    cy.get("tr td:nth-child(2)").then(($cells) => {
+      const found = Array.from($cells).some((cell) => cell.textContent.trim() === username);
+  
+      // Assert that the username exists in the table
+      expect(found).to.be.false;
+    });
+  }
+
   reqFieldCheck() {
     cy.get(".p-error").contains("firstName is required").should("exist");
     cy.get(".p-error").contains("lastName is required").should("exist");
     cy.get(".p-error").contains("email is required").should("exist");
   }
 
+  emailFormatWarning(){
+    cy.get(".p-error").contains("email is not a valid email").should("exist");
+  }
+
+  emailDupMsg(){
+    cy.get(".p-toast-detail").contains("BOLUser already exists").should("exist");
+  }
+
+  mobileDupMsg(){
+    cy.get(".p-toast-detail").contains("mobileNumber: The mobile number has already been taken.").should("exist");
+  }
+
   enterFname(fname) {
+    cy.get("[id='First name']").clear();
     cy.get("[id='First name']").type(fname);
   }
 
   enterLname(lname) {
+    cy.get("[id='Last name']").clear();
     cy.get("[id='Last name']").type(lname);
   }
 
   enterEmail(email) {
+    cy.get("[id='Email Address']").clear();
     cy.get("[id='Email Address']").type(email);
   }
 
   enterMobileNo(mobile) {
+    cy.get("[id='Mobile Number']").clear();
     cy.get("[id='Mobile Number']").type(mobile);
   }
 
@@ -51,14 +96,17 @@ class UserPage {
   }
 
   enterDept(department) {
+    cy.get("#Department").clear();
     cy.get("#Department").type(department);
   }
 
   enterSubDept(sub_department) {
+    cy.get("[id='Sub Department']").clear();
     cy.get("[id='Sub Department']").type(sub_department);
   }
 
   enterPosition(position) {
+    cy.get("#Position").clear();
     cy.get("#Position").type(position);
   }
 
